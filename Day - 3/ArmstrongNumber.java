fd76cf647c9922183b57ce2864ee940a50dc81ed
Ajay Kumar E <154789251+ajay-o5@users.noreply.github.com>
2026-02-08 19:32:36 +0530

    Create ArmstrongNumber.java

---
+++ Day - 3/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int temp = num, sum = 0;
+
+        while(num > 0){
+            int d = num % 10;
+            sum += d * d * d;
+            num /= 10;
+        }
+
+        if(sum == temp)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
