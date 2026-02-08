033fe1a4bf70bfad2fb6199e98b942d4a5610992
Ajay Kumar E <154789251+ajay-o5@users.noreply.github.com>
2026-02-08 19:30:23 +0530

    Create PrimeCheck.java

---
+++ Day - 3/PrimeCheck.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean prime = true;
+
+        if(num <= 1) prime = false;
+
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num % i == 0){
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
