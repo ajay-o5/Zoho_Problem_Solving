9148a2b4b2486122d0b359c526ac85adf633d6f6
Ajay Kumar E <154789251+ajay-o5@users.noreply.github.com>
2026-02-24 20:23:10 +0530

    Create Library System Skeleton.java

---
+++ Day-7/Library System Skeleton.java
@@ -0,0 +1,12 @@
+package oopsassignment;
+import java.util.*;
+
+class Book {}
+class Member {}
+class Library {}
+
+public class LibraryTest {
+    public static void main(String[] args) {
+        System.out.println("Library System Initialized");
+    }
+}
