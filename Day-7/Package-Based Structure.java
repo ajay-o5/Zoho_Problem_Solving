f39096e9f984578610b7211f98856ce2ef36f42d
Ajay Kumar E <154789251+ajay-o5@users.noreply.github.com>
2026-02-24 20:29:10 +0530

    Create Package-Based Structure.java

---
+++ Day-7/Package-Based Structure.java
@@ -0,0 +1,8 @@
+package oopsassignment;
+import java.util.*;
+
+public class PackageDemo {
+    public static void main(String[] args) {
+        System.out.println("Package Working Successfully");
+    }
+}
