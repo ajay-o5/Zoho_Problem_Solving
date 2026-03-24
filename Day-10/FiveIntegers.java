b73ec2ff382ea13d51d891a34a0444a6d61286f0
Ajay Kumar E <154789251+ajay-o5@users.noreply.github.com>
2026-03-24 15:43:10 +0530

    Create FiveIntegers.java

---
+++ Day-10/FiveIntegers.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+public class fiveintegers {
+       public static void main(String[]args) {
+    	   java.util.List<Integer> List = new ArrayList<>();
+    	   List.add(10);
+    	   List.add(20);
+    	   List.add(30);
+    	   List.add(40);
+    	   List.add(50);
+    	   System.out.println("ArrayList contents:"+List);
+    	   System.out.println("First element:"+List.get(0));
+    	   
+       }
+}
